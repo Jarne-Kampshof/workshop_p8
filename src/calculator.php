bd333ed6d953418cd59779ec9f447fa239adb411
Jarne <jarne.kampshof@student.graafschapcollege.nl>
2026-06-11 10:30:24 +0200

    j

--- src/calculator.php
+++ src/calculator.php
@@ -8,7 +8,7 @@ class Calculator
 {
     public function add(float $a, float $b): float
     {
-        return $a + $b;  
+        return $a + $b;
     }
 
     public function subtract(float $a, float $b): float
